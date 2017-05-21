08d2d76647b72af4f6b01d499b2bc6f760ae0e3b
pradeep501 <pradeep501@yahoo.co.in>
2017-05-21 12:57:37 -0500

    Update RemoveCol.R

--- RemoveCol.R
+++ RemoveCol.R
@@ -4,5 +4,5 @@ mydata<-read.csv("dirty_data2.csv",header = TRUE)
 # Remove the "Strange.HTML" Column
 dirty_data3 = subset(mydata,select = -c(Strange.HTML))
 # Write the updated data into csv file
-write.csv(dirty_data3,file = "dirty_data3.csv")
+write.csv(dirty_data3,file = "dirty_data_Cleaned.csv")
 
